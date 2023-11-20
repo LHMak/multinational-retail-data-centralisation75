5a8498056a515628cdc8d58f3318fa56bf073823
LHMak <lhmakhdum@gmail.com>
2023-11-20 11:36:55 +0000

    Reworked main.py and data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,17 +1,14 @@
 # Defining data extractor class
-from database_utils import DatabaseConnector
-from sqlalchemy import create_engine
-from sqlalchemy import inspect
 from sqlalchemy import text
+from sqlalchemy.engine import Engine
 import pandas as pd
 
 class DataExtractor:
-    # Takes list of tables and engine as argument, returns table contents as a Pandas Dataframe
-    def read_rds_table(self, table_list, engine):
-        chosen_table = input(f"Select a table to view from:\n{table_list}: ")
+    # Takes engine and table as arguments, returns table contents as a Pandas Dataframe
+    def read_rds_table(self, engine: Engine, chosen_table):
         with engine.connect() as conn:
             result = conn.execute(text(f"SELECT * FROM {chosen_table}"))
             df_result = pd.DataFrame(result)
             print(df_result.head(10))
             return df_result
-
+    
--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import yaml
-from sqlalchemy import create_engine
-from sqlalchemy import inspect
 from database_utils import DatabaseConnector
 from data_extraction import DataExtractor
 
@@ -8,14 +5,43 @@ from data_extraction import DataExtractor
 connection = DatabaseConnector()
 #instantialising DataExtractor
 extractor = DataExtractor()
+# Assigning database credentials to variable
+legacy_db_creds = 'db_creds.yaml'
 
-# gather credentials for database from file
-creds = connection.read_db_creds('db_creds.yaml')
 
-# create engine from credentials
-engine = connection.init_db_engine(creds)
+# This method displays a list of all tables in db
+# and prompts the user to choose one, returning
+# a Pandas dataframe of the db
+def choose_table():
+    # gather database credentials from file
+    creds = connection.read_db_creds(legacy_db_creds)
+    # create engine from credentials
+    engine = connection.init_db_engine(creds)
+    # list tables from connected database
+    table_list = connection.list_db_tables(engine)
+    chosen_table = input(f"Select a table to view from:\n{table_list}: ")
+    # return dataframe of chosen data contents
+    table_read = extractor.read_rds_table(engine, chosen_table)
+#choose_table()
 
-# list table from connected database
-table_list = connection.list_db_tables(engine)
 
-table_read = extractor.read_rds_table(table_list, engine)
+# Once complete, this function will create a dataframe
+# of the userdata and perform data cleaning on it.
+# Currently, this function creates the dataframe
+# TODO: add data cleaning functionality
+def clean_user_data():
+    # gather database credentials from file
+    creds = connection.read_db_creds(legacy_db_creds)
+    # create engine from credentials
+    engine = connection.init_db_engine(creds)
+    # gather a list of tables in db
+    table_list = connection.list_db_tables(engine)
+    # index list of tables to select user data
+    user_data = table_list[1]
+    # return dataframe of chosen data contents
+    table_read = extractor.read_rds_table(engine, user_data)
+
+clean_user_data()
+
+
+
