4285bd76c65df4d6d80ed48c517de171de3cc398
LHMak <lhmakhdum@gmail.com>
2023-11-21 10:38:11 +0000

    Continues to clean user_data table

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from pandasgui import show
 # Defining data cleaner class
 
 class DataCleaning:
@@ -12,30 +12,30 @@ class DataCleaning:
     def clean_user_data (self, df):
         print("--- raw df info ---")
         df.info()
-        print("\n\n--- Casting string columns ---\n\n")
-        df_string_cols = list(df[['first_name', 'last_name', 'company', 'email_address',
-                             'country', 'country_code', 'user_uuid']])
-        df[df_string_cols] = df[df_string_cols].astype('string')
-        # replace new line char in address column with ', '
-        # down the line, may need to use regex expression instead
 
+        # set index to column 1 and sort by ascending
+        df.set_index(df.columns[0], inplace = True)
+        df.sort_values(by=['index'], inplace = True)
+
+        # Casting string columns
+        df['address'] = df['address'].str.replace('\n', ', ')
         df.info()
+        df_string_cols = list(df[['first_name', 'last_name', 'company', 'email_address',
+                             'country', 'country_code', 'user_uuid', 'address', 'phone_number']])
+        df[df_string_cols] = df[df_string_cols].astype('string')
 
-        print("\n\n--- Casting date columns ---\n\n")
-
+        # Casting date columns
         # using pd.to_datetime to convert date formats, errors return NaT
         df['date_of_birth'] = pd.to_datetime(df['date_of_birth'], infer_datetime_format=True, errors='coerce')
         df['join_date'] = pd.to_datetime(df['join_date'], infer_datetime_format=True, errors='coerce')
-        # removing rows where date_of_birth + join_date are null
+        
+        # removing rows where date_of_birth or join_date are null
         null_dobs = df['date_of_birth'].isnull()
         null_join_dates = df['join_date'].isnull()
-        problem_dates = df[(null_dobs) & (null_join_dates)]
+        problem_dates = df[(null_dobs) | (null_join_dates)]
         df = df.drop(problem_dates.index)
         df.info()
-        print(df)
-        print('cleaning address -------------------')
-
-        print(df['address'].str.split('\n', expand=True))
+        show(df)
 
 
 
