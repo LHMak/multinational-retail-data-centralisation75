4d8833272cbe9d892d9ef81e6bf403f32e69ae72
LHMak <lhmakhdum@gmail.com>
2023-11-27 15:53:04 +0000

    Adds docstrings throughout scripts

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,51 +5,62 @@ import numpy as np
 
 class DataCleaning:
     '''
-    This class.
+    This class is used to clean raw data before it is uploaded to a database.
 
     Functions:
-        x:
+        clean_user_data: cleans raw user data.
+        clean_card_data: cleans raw card data.
+        clean_store_data: cleans raw store data.
+        convert_product_weights: cleans product weight data.
+            convert_to_kg: nested method to convert product weights to kg.
+        clean_product_data: cleans raw product data.
+        clean_orders_data: cleans raw orders data.
+        clean_date_events: cleans raw date events data.
     '''
     def clean_user_data (self, raw_user_data):
         '''
-        This function
+        This function takes in raw user data and cleans it.
+
+        Columns are converted to their intended data types and null values are identified and removed.
 
         Args:
+            raw_user_data: a pandas dataframe containing the raw user data to be cleaned.
         
         Returns:
+            raw_user_data: a pandas dataframe which as now been cleaned. Will be reassigned as clean_user_data in main.py
         '''
         # Sets index to column 1 and sort by ascending
         raw_user_data.set_index(raw_user_data.columns[0], inplace = True)
         raw_user_data.sort_values(by=['index'], inplace = True)
-
         # Casting string columns
         raw_user_data['address'] = raw_user_data['address'].str.replace('\n', ', ')
         string_cols = list(raw_user_data[['first_name', 'last_name', 'company', 'email_address',
                              'country', 'country_code', 'user_uuid', 'address', 'phone_number']])
         raw_user_data[string_cols] = raw_user_data[string_cols].astype('string')
-
         # Casting date columns
         # using pd.to_datetime to convert date formats, errors return NaT
         raw_user_data['date_of_birth'] = pd.to_datetime(raw_user_data['date_of_birth'], infer_datetime_format=True, errors='coerce')
         raw_user_data['join_date'] = pd.to_datetime(raw_user_data['join_date'], infer_datetime_format=True, errors='coerce')
-        
         # removing rows where date_of_birth or join_date are null
         null_dobs = raw_user_data['date_of_birth'].isnull()
         null_join_dates = raw_user_data['join_date'].isnull()
         problem_dates = raw_user_data[(null_dobs) | (null_join_dates)]
         raw_user_data = raw_user_data.drop(problem_dates.index)
-
         # Resets index column on dataframe and returns to main.py
         raw_user_data.reset_index(drop = True, inplace=True)
         return raw_user_data
     
     def clean_card_data(self, raw_card_data):
         '''
-        This function
+        This function takes in raw card data and cleans it.
+
+        Columns are converted to their intended data types and null values are identified and removed.
 
         Args:
+            raw_card_data: a pandas dataframe containing the raw card data to be cleaned.
         
         Returns:
+            raw_card_data: a pandas dataframe which as now been cleaned. Will be reassigned as clean_card_data in main.py
         '''
         # Cleans card_number column by casting as string and removing non numeric characters,
         # then converts to numeric data and finally drops null values
@@ -58,56 +69,62 @@ class DataCleaning:
         null_card_no = raw_card_data.loc[raw_card_data['card_number'].isnull()]
         raw_card_data = raw_card_data.drop(null_card_no.index)
         raw_card_data['card_number'] = raw_card_data['card_number'].astype('int64')
-
         # Casts exp_date as datetime64, drops null values then formats as mm/yy
         raw_card_data['expiry_date'] = pd.to_datetime(raw_card_data['expiry_date'], format='%m/%y', errors='coerce')
         null_exp_date = raw_card_data.loc[raw_card_data['expiry_date'].isnull()]
         raw_card_data = raw_card_data.drop(null_exp_date.index)
         raw_card_data['expiry_date'] = raw_card_data['expiry_date'].dt.strftime('%m/%y')
-
         # Cassts date_payment_confirmed as datetime64, then removing time component
         raw_card_data['date_payment_confirmed'] = pd.to_datetime(raw_card_data['date_payment_confirmed'], format='mixed')
         raw_card_data['date_payment_confirmed'] = raw_card_data['date_payment_confirmed'].dt.date
-        
+    
         # Resets index column on dataframe and returns to main.py
         raw_card_data.reset_index(drop = True, inplace=True)
         return raw_card_data
     
     def clean_store_data(self, raw_store_data):
         '''
-        This function
+        This function takes in raw store data and cleans it.
+
+        Columns are converted to their intended data types and null values are identified and removed.
+        A regex expression is used to identify erroneous data in some columns.
 
         Args:
+            raw_store_data: a pandas dataframe containing the raw store data to be cleaned.
         
         Returns:
+            raw_store_data: a pandas dataframe which as now been cleaned. Will be reassigned as clean_store_data in main.py
         '''
         # Removes 'lat' column as it looks erroneous
         raw_store_data = raw_store_data.drop(['lat'], axis=1)
-
         # Strips leading 'ee' string on continent column, then drops rows without valid continents
         raw_store_data['continent'] = raw_store_data['continent'].str.lstrip('ee')     
         raw_store_data = raw_store_data.drop(raw_store_data[~raw_store_data['continent'].isin(['Europe','America','nan'])].index)
-
         # Converts numeric cols to numeric datatype
         numeric_cols = ['longitude', 'latitude', 'staff_numbers']
         raw_store_data[numeric_cols] = raw_store_data[numeric_cols].astype('string')
         raw_store_data[numeric_cols] = raw_store_data[numeric_cols].replace(r'[^0-9]+', '', regex=True)
         raw_store_data[numeric_cols] = raw_store_data[numeric_cols].apply(pd.to_numeric, errors='coerce')
-        
         # Converts open_date to datetime64 datatype
         raw_store_data['opening_date'] = pd.to_datetime(raw_store_data['opening_date'], format='mixed', errors='coerce')
-        
         # Resets index column on dataframe and returns to main.py
         raw_store_data.reset_index(drop = True, inplace=True)
         return raw_store_data
 
     def convert_product_weights(self, raw_product_data):
         '''
-        This function
+        This function takes in raw product data and cleans the 'weight' column.
+
+        A nested function, convert_to_kg, is defined. This function converts the weights of each product into kg.
+
+        Empty rows are filled with a 'NaN' string, then the nested convert_to_kg function is applied to each product weight.
+        This nested function uses a regex expression to match only a value and a weight unit (e.g. kg, oz, ml). Any erroneous text
 
         Args:
+            raw product_data: a pandas dataframe containing the raw store data to be cleaned.
         
         Returns:
+            raw product_data: a pandas dataframe which as now been cleaned. Will be reassigned as clean_product_data in main.py
         '''
         # Defines function to separate quantity and weight from unit then performs
         # calculations to convert the weight to kg, depending on its unit.
--- database_utils.py
+++ database_utils.py
@@ -13,43 +13,45 @@ class DatabaseConnector:
         read_db_creds: reads the credentials of a database from a YAML file.
         init_db_engine: creates an SQLAlchemy engine object for connecting to a database.
         list_db_tables: lists the tables in a database.
-        upload_to_db: uploads data to a target database
+        upload_to_db: uploads data to a target database.
     '''
-    # Takes config file as argument, returns dict (hopefully) of credentials
-    def read_db_creds(self, config_file):
+
+    def read_db_creds(self, credentials_file):
         '''
-        This function
+        This function is used to read database credentials.
 
         Args:
+            credentials_file: YAML file containing the credentials of the database.
 
         Returns:
+            db_creds: dict of the database credentials
         '''
-        with open(config_file, 'r') as creds:
+        with open(credentials_file, 'r') as creds:
             db_creds = yaml.safe_load(creds)
             return db_creds
         
-    # Creates SQLAlchemy engine from credentials, can be activated to
-    # perform queries (hopefully)
     def init_db_engine(self, db_creds: dict):
         '''
-        This function
+        This function creates an SQLAlchemy engine from the database credentials dict.
 
         Args:
+            db_creds: Python dict returned from the read_db_creds function.
         
         Returns:
+            engine: SQLAlchemy engine object allowing connection to a database.
         '''
         engine = create_engine(f"postgresql+psycopg2://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}")  
         return engine
     
-    # Connects to the database using the engine and prints a list of
-    # the tables within
     def list_db_tables(self, engine: Engine):
         '''
-        This function
+        This function connects to a database and lists the tables it contains.
 
         Args:
+            engine: SQLAlchemy engine object returned from init_db_engine.
         
         Returns:
+            inspector.get_table_names(): a list of tables within the database.
         '''
         with engine.connect() as conn:
             inspector = inspect(engine)
@@ -57,10 +59,11 @@ class DatabaseConnector:
     
     def upload_to_db(self, engine: Engine, dataframe: DataFrame, table_name: str):
         '''
-        This function
+        This function creates a table in the connected database.
 
         Args:
-        
-        Returns:
+            engine: SQLAlchemy engine object returned from init_db_engine.
+            dataframe: a pandas dataframe of business data.
+            table_name: a string representing the name of the table to be created.
         '''
         dataframe.to_sql(table_name, engine, if_exists='replace')
